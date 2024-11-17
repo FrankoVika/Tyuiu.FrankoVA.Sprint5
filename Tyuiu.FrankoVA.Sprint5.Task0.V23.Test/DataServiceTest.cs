c76beeb560c892e19c6b48bbce6c1599887f2fc4
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-11-17 19:59:37 +0500

    Task0

--- Tyuiu.FrankoVA.Sprint5.Task0.V23.Test/DataServiceTest.cs
+++ Tyuiu.FrankoVA.Sprint5.Task0.V23.Test/DataServiceTest.cs
@@ -10,10 +10,20 @@ namespace Tyuiu.FrankoVA.Sprint5.Task0.V23.Test
         public void Test()
         {
             string path = @"C:\Пользователи\1\source\repos\Tyuiu.FrankoVA.Sprint5\Tyuiu.FrankoVA.Sprint5\Tyuiu.FrankoVA.Sprint5.Task0.V23\bin\Debug\OutOutFileTask0.txt";
+            string tempPath = Path.GetTempPath();
+
+
+            string fileName = "OutOutFileTask0.txt";
+            string p = Path.Combine(tempPath, fileName);
+
             FileInfo fileInfo = new FileInfo(path);
+
+            
             bool fileExists = fileInfo.Exists;
-            bool wait = true;
-            Assert.AreEqual(wait, fileInfo.Exists);
+
+           
+            bool wait = true; 
+            Assert.AreEqual(wait, fileExists);
         }
     }
 }
