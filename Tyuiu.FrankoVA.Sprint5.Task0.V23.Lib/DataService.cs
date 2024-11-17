07ca4598571879b277d9de8598d1100455f63b63
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-11-17 19:47:41 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.FrankoVA.Sprint5.Task0.V23.Lib/DataService.cs
@@ -0,0 +1,19 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.FrankoVA.Sprint5.Task0.V23.Lib
+{
+    public class DataService : ISprint5Task0V23
+    {
+        public string SaveToFileTextData(int x)
+        {
+            string path = $@"{Directory.GetCurrentDirectory()}\OutOutFileTask0.txt";
+            double y = ((1+Math.Pow(x,3) )/(Math.Pow(x,2)));
+            y = Math.Round(y, 3);
+            File.WriteAllText(path, Convert.ToString(y));
+            return path;
+        }
+    }
+}
+
+
+   
---
+++ Tyuiu.FrankoVA.Sprint5.Task0.V23.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using System.Diagnostics;
+using Tyuiu.FrankoVA.Sprint5.Task0.V23.Lib;
+using System.IO;
+namespace Tyuiu.FrankoVA.Sprint5.Task0.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void SaveToFileTextData()
+        {
+            string path = @"C:\Пользователи\1\source\repos\Tyuiu.FrankoVA.Sprint5\Tyuiu.FrankoVA.Sprint5\Tyuiu.FrankoVA.Sprint5.Task0.V23\bin\Debug\OutOutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileInfo.Exists);
+        }
+    }
+}
