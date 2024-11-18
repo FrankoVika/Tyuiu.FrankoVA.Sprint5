1e12b421ddffa4d1729e07c2ce3414e6e9bf011d
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-11-18 20:22:24 +0500

    Task1

---
+++ Tyuiu.FrankoVA.Sprint5.Task1.V27.Test/DataServiceTest.cs
@@ -0,0 +1,28 @@
+using System.IO;
+using Tyuiu.FrankoVA.Sprint5.Task1.V27.Lib;
+namespace Tyuiu.FrankoVA.Sprint5.Task1.V27.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = @"C:\Пользователи\1\source\repos\Tyuiu.FrankoVA.Sprint5\Tyuiu.FrankoVA.Sprint5\Tyuiu.FrankoVA.Sprint5.Task1.V27\bin\Debug\OutOutFileTask1.txt";
+            string tempPath = Path.GetTempPath();
+
+
+            string fileName = "OutOutFileTask0.txt";
+            string p = Path.Combine(tempPath, fileName);
+
+            FileInfo fileInfo = new FileInfo(path);
+
+
+            bool fileExists = fileInfo.Exists;
+
+
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
