3c183639f5e68cdcd0ce7d4e908fa574fa0a29b0
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-11-18 19:43:28 +0500

    Task0

--- Tyuiu.FrankoVA.Sprint5.Task0.V23.Lib/DataService.cs
+++ Tyuiu.FrankoVA.Sprint5.Task0.V23.Lib/DataService.cs
@@ -6,7 +6,7 @@ namespace Tyuiu.FrankoVA.Sprint5.Task0.V23.Lib
     {
         public string SaveToFileTextData(int x)
         {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "OutOutFileTask0.txt");
+            string path = Path.GetTempFileName();
             double y = ((1+Math.Pow(x,3) )/(Math.Pow(x,2)));
             y = Math.Round(y, 3);
             File.WriteAllText(path, Convert.ToString(y));
