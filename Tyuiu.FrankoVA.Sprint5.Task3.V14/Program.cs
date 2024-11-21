6ae929446523fe9d7af271ce406208188bf86602
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-11-21 20:02:43 +0500

    Task3

---
+++ Tyuiu.FrankoVA.Sprint5.Task3.V14/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.FrankoVA.Sprint5.Task3.V14.Lib;
+namespace Tyuiu.FrankoVA.Sprint5.Task3.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5| Выполнила: Франко В.А. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнила: Франко В.А. | ИБКСб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при x = 3, результат сохранить в *");
+            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль.                *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            int x = 3;
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.SaveToFileTextData(x);
+            Console.WriteLine("Файл" + res);
+            Console.WriteLine("Создан");
+            Console.ReadKey();
+        }
+    }
+}
