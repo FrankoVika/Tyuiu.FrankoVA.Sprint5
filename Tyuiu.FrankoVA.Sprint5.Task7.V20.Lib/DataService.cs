6573cfce60160e115404e709555bcbb621f5693e
Tyuiu.FrankoVA@outlook.com <angelcosmo16@gmail.com>
2024-11-28 20:35:23 +0500

    Task7

---
+++ Tyuiu.FrankoVA.Sprint5.Task7.V20.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.FrankoVA.Sprint5.Task7.V20.Lib
+{
+    public class DataService : ISprint5Task7V20
+    {
+        public string LoadDataAndSave(string path)
+        {
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            string paths = Path.Combine(Path.GetTempPath(), "DataSprint5", "OutPutDataFileTask7V20.txt");
+
+
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+
+                    line = line.Replace("Сс", "С").Replace("сс", "с");
+
+
+
+                    File.WriteAllText(paths, line);
+                }
+            }
+
+            return path;
+        }
+    }
+}
+
---
+++ Tyuiu.FrankoVA.Sprint5.Task7.V20/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using Tyuiu.FrankoVA.Sprint5.Task7.V20.Lib;
+namespace Tyuiu.FrankoVA.Sprint5.Task7.V20
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #5| Выполнила: Франко В.А. | ИБКСб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #5                                                               *");
+            Console.WriteLine("* Тема: Класс File. Запись данных в текстовый файл                        *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #23                                                             *");
+            Console.WriteLine("* Выполнила: Франко В.А. | ИБКСб-24-1                                     *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дано выражение, вычислить его значение при x = 3, результат сохранить в *");
+            Console.WriteLine("* текстовый файл OutPutFileTask0.txt и вывести на консоль.                *");
+            Console.WriteLine("* Округлить до трёх знаков после запятой                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            DataService ds = new DataService();
+            string path = Path.Combine(Path.GetTempPath(), "DataSprint5", "InPutDataFileTask7V20.txt");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            string res = ds.LoadDataAndSave(path);
+            Console.WriteLine(res);
+            Console.ReadKey();
+        }
+    }
+}
